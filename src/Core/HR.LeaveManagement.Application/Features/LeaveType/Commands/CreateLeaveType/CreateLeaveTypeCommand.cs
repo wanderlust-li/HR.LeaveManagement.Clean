eb6d64c56eb0b20bc50e2db75d09ef401dbe40c4
wanderlust-li <zhekalichm4n@icloud.com>
2023-09-23 22:19:05 +0300

    add create leave type

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommand : IRequest<int>
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public int DefaultDays { get; set; }
+}
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+        return leaveTypeToCreate.Id;
+    }
+}
