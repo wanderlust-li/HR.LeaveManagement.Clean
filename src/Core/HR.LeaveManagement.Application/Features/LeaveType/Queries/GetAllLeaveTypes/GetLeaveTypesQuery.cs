d56ee6b1a32a9d4beef371ceaa977bb3537efc6b
wanderlust-li <zhekalichm4n@icloud.com>
2023-09-23 15:17:12 +0300

    Creating CQRS Query Request and Handler

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public record GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>;
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler: IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+    
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
+        
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        return data;
+    }
+}
