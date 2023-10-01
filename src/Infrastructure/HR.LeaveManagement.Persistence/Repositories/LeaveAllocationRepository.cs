54967f31ab116008dfefb1afaec719d3b1990690
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-01 11:21:17 +0300

    update LeaveAllocationRepository

--- src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
+++ src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -11,4 +11,49 @@ public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILe
     {
         
     }
+
+    public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+    {
+        var leaveAllocation = await _context.LeaveAllocations
+            .Include(q => q.LeaveType)
+            .FirstOrDefaultAsync(q => q.Id == id);
+
+        return leaveAllocation;
+    }
+
+    public async Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails()
+    {
+        var leaveAllocations = await _context.LeaveAllocations
+            .Include(q => q.LeaveType)
+            .ToListAsync();
+
+        return leaveAllocations;
+    }
+
+    public async Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails(string userId)
+    {
+        var leaveAllocations = await _context.LeaveAllocations
+            .Include(q => q.LeaveType)
+            .ToListAsync();
+
+        return leaveAllocations;
+    }
+
+    public async Task<bool> AllocationExists(string userId, int leaveTypeId, int period)
+    {
+        return await _context.LeaveAllocations.AnyAsync(q => q.EmployeeId == userId
+                                                             && q.LeaveTypeId == leaveTypeId && q.Period == period);
+    }
+
+    public async Task AddAllocations(List<LeaveAllocation> allocations)
+    {
+        await _context.AddRangeAsync(allocations);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<LeaveAllocation> GetUserAllocations(string userId, int leaveTypeId)
+    {
+        return await _context.LeaveAllocations.FirstOrDefaultAsync(q => q.EmployeeId == userId
+                                                                        && q.LeaveTypeId == leaveTypeId);
+    }
 }
