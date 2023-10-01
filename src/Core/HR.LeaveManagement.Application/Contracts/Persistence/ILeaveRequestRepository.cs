6df64ad12b4c9b78f43d3e403df38af1bae1736c
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-01 11:13:07 +0300

    update LeaveRequestRepository

--- src/Core/HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ src/Core/HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,5 +4,9 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 
 public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
 {
+    Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
     
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+    
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId);
 }
--- src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -11,4 +11,31 @@ public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveReq
     {
         
     }
+
+    public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+    {
+        var leaveRequest = await _context.LeaveRequests
+            .Include(q => q.LeaveType)
+            .FirstOrDefaultAsync(q => q.Id == id);
+
+        return leaveRequest;
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+    {
+        var leaveRequests = await _context.LeaveRequests
+            .Include(q => q.LeaveType)
+            .ToListAsync();
+        
+        return leaveRequests;
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+    {
+        var leaveRequests = await _context.LeaveRequests
+            .Where(q => q.RequestingEmployeeId == userId)
+            .Include(q => q.LeaveType).ToListAsync();
+        
+        return leaveRequests;
+    }
 }
