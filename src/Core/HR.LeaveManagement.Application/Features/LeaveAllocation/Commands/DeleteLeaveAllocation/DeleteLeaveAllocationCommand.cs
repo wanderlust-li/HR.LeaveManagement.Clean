8a3b7c478f68101aa1a2481df47bed7715630eea
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-07 01:58:16 +0300

    add command deleteLeaveAllocation

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation;
+
+public class DeleteLeaveAllocationCommand : IRequest
+{
+    public int Id { get; set; }
+}
