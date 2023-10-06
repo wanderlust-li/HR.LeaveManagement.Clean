e735f1426c7f97583f362520d3b7159b95cc5738
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-06 22:07:25 +0300

    add query getLeaveAllocations

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+
+public class GetLeaveAllocationListHandler : IRequestHandler<GetLeaveAllocationListQuery, List<LeaveAllocationDto>>
+{
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+    private readonly IMapper _mapper;
+
+    public GetLeaveAllocationListHandler(ILeaveAllocationRepository leaveAllocationRepository,
+        IMapper mapper)
+    {
+        _leaveAllocationRepository = leaveAllocationRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+    {
+        var leaveAllocations = await _leaveAllocationRepository.GetLeaveAllocationsWithDetails();
+        var allocations = _mapper.Map<List<LeaveAllocationDto>>(leaveAllocations);
+
+        return allocations;
+    }
+}
