f8e5f0deabac3c8a53e167fef86e573b901cef7b
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-07 01:57:09 +0300

    add command createLeaveAllocation

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+
+public class CreateLeaveAllocationCommand : IRequest<Unit>
+{
+    public int LeaveTypeId { get; set; }
+}
