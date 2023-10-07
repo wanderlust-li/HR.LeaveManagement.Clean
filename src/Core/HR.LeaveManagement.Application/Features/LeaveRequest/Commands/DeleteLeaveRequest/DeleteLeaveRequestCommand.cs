47fae8d13ac84b7ec20b302bd1eb6a97c0ce1af3
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-07 18:11:53 +0300

    Add update and delete commands in LeaveAllocation

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/DeleteLeaveRequest/DeleteLeaveRequestCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest;
+
+public class DeleteLeaveRequestCommand : IRequest
+{
+    public int Id { get; set; }
+}
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Shared/BaseLeaveRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
+
+public class BaseLeaveRequest
+{
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    public int LeaveTypeId { get; set; }
+}
