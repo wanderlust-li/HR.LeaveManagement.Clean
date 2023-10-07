12a13de9b135c7a11669113d73e2e7a513be2232
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-07 18:15:38 +0300

    complete leave request feature

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/ChangeLeaveRequestApproval/ChangeLeaveRequestApprovalCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Emails;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+
+public class ChangeLeaveRequestApprovalCommandHandler : IRequestHandler<ChangeLeaveRequestApprovalCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly IEmailSender _emailSender;
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public ChangeLeaveRequestApprovalCommandHandler(
+        ILeaveRequestRepository leaveRequestRepository, 
+        ILeaveTypeRepository leaveTypeRepository, 
+        IMapper mapper, IEmailSender emailSender)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _leaveTypeRepository = leaveTypeRepository;
+        _mapper = mapper;
+        this._emailSender = emailSender;
+    }
+
+    public async Task<Unit> Handle(ChangeLeaveRequestApprovalCommand request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+        if (leaveRequest is null)
+            throw new NotFoundException(nameof(LeaveRequest), request.Id);
+        
+        leaveRequest.Approved = request.Approved;
+        await _leaveRequestRepository.UpdateAsync(leaveRequest);
+        
+        var email = new EmailMessage
+        {
+            To = string.Empty, 
+            Body = $"The approval status for your leave request for {leaveRequest.StartDate:D} to {leaveRequest.EndDate:D} " +
+                   $"has been updated.",
+            Subject = "Leave Request Approval Status Updated"
+        };
+
+        await _emailSender.SendEmail(email);
+
+        return Unit.Value;
+    }
+}
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/ChangeLeaveRequestApproval/ChangeLeaveRequestApprovalCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+
+public class ChangeLeaveRequestApprovalCommandValidator : AbstractValidator<ChangeLeaveRequestApprovalCommand>
+{
+    public ChangeLeaveRequestApprovalCommandValidator()
+    {
+        RuleFor(p => p.Approved)
+            .NotNull()
+            .WithMessage("Approval status cannot be null");
+    }
+}
