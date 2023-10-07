30ae666f74668a9a417142263a2e750708841c4a
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-07 17:59:15 +0300

    add query GetLeaveRequestList

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestList/GetLeaveRequestListQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+
+public class GetLeaveRequestListQuery : IRequest<List<LeaveRequestListDto>>
+{
+}
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestList/LeaveRequestListDto.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+
+public class LeaveRequestListDto
+{
+    public string RequestingEmployeeId { get; set; }
+    
+    public LeaveTypeDto LeaveType { get; set; }
+    
+    public DateTime DateRequested { get; set; }
+    
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    public bool? Approved { get; set; }
+}
