7e6a1c37d4430ec853fef914389bf73e1bb72874
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-11 12:33:10 +0300

    Global Error Handling

--- src/API/HR.LeaveManagement.Api/Program.cs
+++ src/API/HR.LeaveManagement.Api/Program.cs
@@ -1,3 +1,4 @@
+using HR.LeaveManagement.Api.Middleware;
 using HR.LeaveManagement.Application;
 using HR.LeaveManagement.Infrastructure;
 using HR.LeaveManagement.Persistence;
@@ -22,6 +23,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -31,7 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("all");
+// app.UseCors("all");
 
 app.UseAuthorization();
 
